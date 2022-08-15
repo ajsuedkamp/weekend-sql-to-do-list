2e51eb267feea2288c86da4d22105c4521924892
Alex Suedkamp <ajsuedkamp@gmail.com>
2022-08-15 17:19:05 -0500

    created get request

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -4,8 +4,31 @@ $(readyNow);
 
 function readyNow() {
     console.log('JQ sourced');
+    $('#add-task').on('click', postTasks);
+    getTasks();
 }
 
 function getTasks() {
+    $.ajax({
+        type: 'GET',
+        url: '/tasks'
+    }).then(function (response) {
+        console.log(response)
+        for(let item of response) {
+            $('#taskTableBody').append(`
+                 <tr>
+                    <td>${item.task}</td>
+                    <td>${item.complete}</td>
+                </tr>
+            `);
+        }
+    }).catch(function (error) { // Reject / failure
+        console.log(error);
+        alert('Something went wrong!');
+    });
+} //End getTasks function
+
+function postTasks() {
     
 }
+
--- server/server.js
+++ server/server.js
@@ -5,7 +5,16 @@ const PORT = process.env.PORT || 5000;
 app.use(express.static('server/public'));
 app.use(express.urlencoded({ extended: true }));
 
+let taskArray = [
+    {
+        task: 'sweep the floor',
+        complete: 'True'
+    }
+]
 
+app.get('/tasks', (req, res) => {
+    res.send(taskArray);
+});
 
 
 app.listen(PORT, () => {
